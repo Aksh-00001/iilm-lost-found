06e2bea802a56a16c003f3aba207cbcea91ff049
Aksh raj <akshrajsingh99310@gmail.com>
2026-02-18 19:42:29 +0530

    connect frontend to backend

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
+  origin: process.env.CLIENT_URL || 'https://iilm-lost-found.vercel.app',
   credentials: true
 }));
 app.use(express.json({ limit: '10mb' }));
